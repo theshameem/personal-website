e5c3e97fd772434b31162888211a0f9df43ff769
theshameem <theshameem.alam@gmail.com>
2023-07-12 15:58:24 +0600

    feat: components and pages setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./App.css";
 
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Portfolio from "./pages/Portfolio/Portfolio";
+import Contact from "./pages/Contact/Contact";
+
 function App() {
   return <></>;
 }
