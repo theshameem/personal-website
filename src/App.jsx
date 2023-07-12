d670dbc69b66e3dfd49a1ac2a20371bf414267c2
theshameem <theshameem.alam@gmail.com>
2023-07-12 16:13:55 +0600

    feat: browser router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
-import Portfolio from "./pages/Portfolio/Portfolio";
 import Contact from "./pages/Contact/Contact";
+import Home from "./pages/Home/Home";
+import Portfolio from "./pages/Portfolio/Portfolio";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
