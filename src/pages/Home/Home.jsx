91af333be369ccfaaae0c20149be3284f5a6c736
theshameem <theshameem.alam@gmail.com>
2023-07-13 18:43:35 +0600

    feat: home page implementation

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { FaArrowRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import ProfileImage from "../../assets/shameem_alam.png";
+import "./Home.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <section className="home section grid">
+      <img src={ProfileImage} alt="profile-image" className="h-img" />
+
+      <div className="h-content">
+        <div className="h-data">
+          <h1 className="h-title">
+            <span>I'm Shameem Alam.</span> Software Engineer
+          </h1>
+
+          <p className="h-description">
+            I'm a dedicated front-end developer with a passion for learning and
+            staying up-to-date with new technologies. With a focus on creating
+            beautiful and user-friendly websites, I strive to continuously
+            enhance my skills and explore innovative approaches to deliver
+            exceptional digital experiences.
+          </p>
+
+          <Link to="/about" className="button">
+            More About Me{" "}
+            <span className="button-icon">
+              <FaArrowRight />
+            </span>
+          </Link>
+        </div>
+      </div>
+
+      <div className="color-block"></div>
+    </section>
+  );
 }
 
 export default Home;
