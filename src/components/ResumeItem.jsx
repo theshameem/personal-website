1d9b9914d787c5f8fbd8fdc2e94c77bc3f047939
theshameem <theshameem.alam@gmail.com>
2023-07-16 20:53:04 +0600

    feat: resume item component implementation

---
+++ src/components/ResumeItem.jsx
@@ -0,0 +1,16 @@
+import parse from "html-react-parser";
+import React from "react";
+
+function ResumeItem({ icon, year, title, desc }) {
+  return (
+    <div className="resume-item">
+      <div className="resume-icon">{icon}</div>
+
+      <span className="resume-date">{year}</span>
+      <h3 className="resume-subtitle">{parse(title)}</h3>
+      <p className="resume-description">{desc}</p>
+    </div>
+  );
+}
+
+export default ResumeItem;
