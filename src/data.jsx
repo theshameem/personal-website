6bb3d2b2897b1c8f0b9588532a60484beb9abec4
theshameem <theshameem.alam@gmail.com>
2023-08-06 21:23:01 +0600

    fix: themes data remove

--- src/data.jsx
+++ src/data.jsx
@@ -16,17 +16,6 @@ import Work4 from "./assets/project-4.jpeg";
 import Work5 from "./assets/project-5.jpg";
 import Work6 from "./assets/project-6.jpg";
 
-import Theme4 from "./assets/blue.png";
-import Theme3 from "./assets/blueviolet.png";
-import Theme5 from "./assets/goldenrod.png";
-import Theme9 from "./assets/green.png";
-import Theme6 from "./assets/magenta.png";
-import Theme8 from "./assets/orange.png";
-import Theme1 from "./assets/purple.png";
-import Theme2 from "./assets/red.png";
-import Theme10 from "./assets/yellow.png";
-import Theme7 from "./assets/yellowgreen.png";
-
 export const links = [
   {
     id: 1,
@@ -416,67 +405,5 @@ export const portfolio = [
         desc: "www.dibble.com",
       },
     ],
-  },
-];
-
-export const themes = [
-  {
-    id: 1,
-    img: Theme1,
-    color: "hsl(252, 35%, 51%)",
-  },
-
-  {
-    id: 2,
-    img: Theme2,
-    color: "hsl(4, 93%, 54%)",
-  },
-
-  {
-    id: 3,
-    img: Theme3,
-    color: "hsl(271, 76%, 53%)",
-  },
-
-  {
-    id: 4,
-    img: Theme4,
-    color: "hsl(225, 73%, 57%)",
-  },
-
-  {
-    id: 5,
-    img: Theme5,
-    color: "hsl(43, 74%, 49%)",
-  },
-
-  {
-    id: 6,
-    img: Theme6,
-    color: "hsl(339, 81%, 66%)",
-  },
-
-  {
-    id: 7,
-    img: Theme7,
-    color: "hsl(80, 61%, 50%)",
-  },
-
-  {
-    id: 8,
-    img: Theme8,
-    color: "hsl(19, 96%, 52%)",
-  },
-
-  {
-    id: 9,
-    img: Theme9,
-    color: "hsl(88, 65%, 43%)",
-  },
-
-  {
-    id: 10,
-    img: Theme10,
-    color: "hsl(42, 100%, 50%)",
   },
 ];
